bb03dbb84a3d68b73b3da86c255d3df13576ea65
Robert Broden <Robert.Broden@newmarketinc.com>
2017-12-23 11:02:42 -0500

    initial commit

---
+++ src/lib/test/test.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'ah-test',
+    template: '<span>Test</span>',
+    styleUrls: ['./test.css']
+})
+export class TestComponent {}
